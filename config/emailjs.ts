cb586e8cfd1511f8d215c8278b77efbbd5d8a471
JnT <minhthong19042002@gmail.com>
2026-01-17 09:26:49 +0700

    Fix EmailJS Template ID: urm90s4 -> template_8kw16lm

--- config/emailjs.ts
+++ config/emailjs.ts
@@ -2,6 +2,6 @@
 // Note: These are PUBLIC keys designed to be exposed on the client-side
 export const EMAILJS_CONFIG = {
   serviceId: 'service_g9022wi',
-  templateId: 'urm90s4',
+  templateId: 'template_8kw16lm',
   publicKey: 'FWWsl-ip800NAouAY',
 } as const
